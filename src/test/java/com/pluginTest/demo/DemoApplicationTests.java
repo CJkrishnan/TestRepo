a1d977a6f72ee348bbfe5a1e993db114d573cbee
Jayakumar Krishnan <jayakumar.k@logicfocus.net>
2022-08-04 11:33:44 +0530

    First commit

---
+++ src/test/java/com/pluginTest/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluginTest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
